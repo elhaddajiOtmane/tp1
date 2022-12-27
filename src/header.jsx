02de5878cfba745529742bb93ac806f5b0c8ccea
ELHADADJI otmane <39245156+elhaddajiOtmane@users.noreply.github.com>
2022-12-27 09:05:53 +0100

    Create header.jsx

---
+++ src/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+//  tailwindcss import
+import './styles.css'
+
+
+export default function header() {
+  return (
+    <div><h1>header</h1>
+    <div class="bg-red-500">This element has a red background</div>
+    
+    </div>
+    
+  )
+}
